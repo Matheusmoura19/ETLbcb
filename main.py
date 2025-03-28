8785850d8b4e1646060391ac9753b0a4a2e7ed5d
Matheusmoura19 <163340353+Matheusmoura19@users.noreply.github.com>
2025-03-28 15:59:10 -0300

    Aula 2
    Código implementado durante a aula de Data Science.

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-import requests
 import pandas as pd
+from src.extractTransform import requestApiBcb
+from src.load import salvarCsv
 
-def busca_data(data):
-    """
-    Função para extrair os dados dos meios de pagamento 
-    
-    """
-    url = f'https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json'
-
-    req = requests.get(url)
-    dados = req.json()
-
-    df = pd.json_normalize(dados['value'])
-
-    return print(df)
-
-
-
-busca_data('20241')
+dados_bcb = requestApiBcb('20191')
+salvarCsv(dados_bcb, 'src/datasets/meiosPagamentosTri.csv', ';', '.')
