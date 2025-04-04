e6cd4fac36a3812d6839e85196799168f82a16c1
Matheusmoura19 <163340353+Matheusmoura19@users.noreply.github.com>
2025-04-04 15:55:19 -0300

    Conteúdo da Aula 3

--- src/load.py
+++ src/load.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
+
 
 def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str):
     """
@@ -11,8 +14,27 @@ def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str)
         decimal (str): Caractere usado para representar o ponto decimal (ex: '.' ou ',').
 
     Saída esperada:
-    O DataFrame deve ser salvo em um arquivo .csv
-    
+    O DataFrame deve ser salvo em um arquivo .csv dentro da pasta de datasets.
+
     """
-    df.to_csv(nome_arquivo, sep=separador, decimal=decimal)
+    df.to_csv(nome_arquivo, sep=separador, decimal= decimal)
     return
+
+
+def salvarSQLite(df: pd.DataFrame, nome_banco: str, nome_tabela: str):
+
+    conn = sqlite3.connect(nome_banco)
+
+    df.to_sql(nome_tabela, conn, if_exists="replace", index=False)
+
+    conn.close()
+
+    return
+
+
+def salvarMySql(
+    df: pd.DataFrame, usuario: str, senha: str, host: str, banco: str, nome_tabela: str
+):
+    engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@{host}:{banco}")
+
+    df.to_sql(nome_tabela, con=engine, if_exists="replace", index=False)
